69560843879522e31e7e174eda50db028cc3a6f8
Natalie Eng <natalie.eng@wustl.edu>
2022-10-15 18:54:01 -0500

    Added habits.

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,8 @@ routes_app = Blueprint('routes', __name__)
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+
+@routes_app.route('/inputHabits')
+def inputHabits():
+    return render_template('habits.html', title='Habits', user=user)
